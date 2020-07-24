623f3f21c27fa3fe98ba14bddbbb2508bda8cf86
Mate Csornyei <mate@csornyei.com>
2020-07-24 13:46:19 +0200

    Requesting golya data fix

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,33 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router,
          Switch,
          Route
 } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import axios, {AxiosResponse} from 'axios';
 import AdminNavbar from './Components/Navbar';
 import Golyak from './Components/GolyaPage/Golyak';
-import { golyaRequest } from './State/actions';
 
 function App() {
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    axios.get('http://teszt.api.bceocsi.com/golya/read.php').then((response: AxiosResponse) => {
-      const golyaAdatok = response.data.records;
-      const javitottGolyaAdatok = golyaAdatok.map((golya: any) => {
-        golya.hetfo = golya.hetfo === "1";
-        golya.kedd = golya.kedd === "1";
-        golya.szerda = golya.szerda === "1";
-        golya.csutortok = golya.csutortok === "1";
-        golya.pentek = golya.pentek === "1";
-
-        return golya;
-      })
-      dispatch(golyaRequest(javitottGolyaAdatok))
-    })
-  }, []);
 
   return (
     <Router>
--- admin/src/Components/GolyaPage/Golyak.tsx
+++ admin/src/Components/GolyaPage/Golyak.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import { useDispatch } from 'react-redux';
+import axios, {AxiosResponse} from 'axios';
+import ocsiApi from '../../API/ocsiApi';
+import { golyaRequest } from '../../State/actions';
 
 import Control from './Control';
 import Filters from './Filters';
 import GolyaList from './GolyaList';
 
 const Golyak = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        ocsiApi.get('/golya/read.php').then((response: AxiosResponse) => {
+          const golyaAdatok = response.data.records;
+          const javitottGolyaAdatok = golyaAdatok.map((golya: any) => {
+            golya.hetfo = golya.hetfo === "1";
+            golya.kedd = golya.kedd === "1";
+            golya.szerda = golya.szerda === "1";
+            golya.csutortok = golya.csutortok === "1";
+            golya.pentek = golya.pentek === "1";
+
+            return golya;
+          })
+          dispatch(golyaRequest(javitottGolyaAdatok))
+        })
+      }, []);
 
     // BLOCKED - Request data -> API needed
     // DONE - Show data
