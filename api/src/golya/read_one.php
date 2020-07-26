0eb61b208c594627ec274b3d54edf5b5c34dbd61
Mate Csornyei <mate@csornyei.com>
2020-07-26 18:24:45 +0200

    API Header fixes

--- api/src/golya/read_one.php
+++ api/src/golya/read_one.php
@@ -1,10 +1,30 @@
 <?php
-    // required headers
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: access");
-    header("Access-Control-Allow-Methods: GET");
-    header("Access-Control-Allow-Credentials: true");
-    header('Content-Type: application/json');
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+
+        echo json_encode(
+            array("message" => "ok")
+        );
+        return;
+    }
 
     // include database and object files
     include_once '../config/database.php';
